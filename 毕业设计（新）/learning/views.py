3904996f0ea768f35b91a1b5630def8cb6f351d3
youke2002 <134725731+youke2002@users.noreply.github.com>
2025-05-06 22:43:36 +0800

    Delete 毕业设计（新） directory

--- 毕业设计（新）/learning/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Course
-
-def course_list(request):
-    courses = Course.objects.all()
-    return render(request, 'learning/course_list.html', {'courses': courses})
